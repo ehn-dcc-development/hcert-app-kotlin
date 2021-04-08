71e56659ab3f2ddbb4747791c8286ec57082edbd
Christian Kollmann <christian.kollmann@a-sit.at>
2021-04-08 16:45:14 +0200

    Scan QR Code

--- app/src/main/java/ehn/techiop/hcert/android/MainActivity.kt
+++ app/src/main/java/ehn/techiop/hcert/android/MainActivity.kt
@@ -1,11 +1,15 @@
 package ehn.techiop.hcert.android
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +19,22 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            val intentIntegrator = IntentIntegrator(this)
+            intentIntegrator.setOrientationLocked(true)
+            intentIntegrator.setDesiredBarcodeFormats(BarcodeFormat.AZTEC.name, BarcodeFormat.QR_CODE.name)
+            val createScanIntent = intentIntegrator.createScanIntent()
+            startActivityForResult(createScanIntent, IntentIntegrator.REQUEST_CODE)
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IntentIntegrator.REQUEST_CODE) {
+            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)?.let {
+                Log.i("MainActivity","Content: ${it.contents}")
+            }
         }
     }
 
