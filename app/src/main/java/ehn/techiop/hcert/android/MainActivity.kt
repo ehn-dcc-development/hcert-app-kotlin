63a8a7916f8dd10ea7636495b61f84f11f23fff6
Christian Kollmann <christian.kollmann@a-sit.at>
2021-04-09 08:19:29 +0200

    Print details about scanned data

--- app/src/main/java/ehn/techiop/hcert/android/MainActivity.kt
+++ app/src/main/java/ehn/techiop/hcert/android/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -35,17 +36,24 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         IntentIntegrator.parseActivityResult(requestCode, resultCode, data)?.let { intentResult ->
             intentResult.contents?.let {
+                findViewById<TextView>(R.id.textview_first).text = "Validating ..."
+                findViewById<LinearLayout>(R.id.container_data).removeAllViews()
                 thread {
                     try {
                         val vaccinationData = getChain().verify(it)
                         runOnUiThread {
-                            findViewById<TextView>(R.id.textview_first).text =
-                                "Successfully validated this input: $vaccinationData"
+                            findViewById<TextView>(R.id.textview_first).text = ""
+                            fillLayout(
+                                findViewById<LinearLayout>(R.id.container_data),
+                                vaccinationData
+                            )
                         }
                     } catch (e: Throwable) {
                         runOnUiThread {
-                            findViewById<TextView>(R.id.textview_first).text =
-                                "Error on validation: ${e.message}"
+                            findViewById<LinearLayout>(R.id.container_data).addView(TextView(this).also {
+                                it.text = "Error on validation: ${e.message}"
+                                it.setTextColor(resources.getColor(R.color.error, theme))
+                            })
                         }
                     }
                 }
@@ -53,7 +61,91 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getChain() = CborVerificationChain(
+    private fun fillLayout(container: LinearLayout, data: VaccinationData) {
+        container.removeAllViews()
+        container.addView(TextView(this).also {
+            it.text = "Successfully validated the scanned code."
+            it.setTextColor(resources.getColor(R.color.success, theme))
+        })
+        data.sub?.let { sub ->
+            fillSubject(container, sub)
+        }
+        data.rec?.let { rec ->
+            fillRecovery(container, rec)
+        }
+        data.tst?.let { tst ->
+            fillTest(container, tst)
+        }
+        data.vac?.let { vacList ->
+            vacList.forEach { entry ->
+                entry?.let { vac ->
+                    fillVac(container, vac)
+                }
+            }
+        }
+    }
+
+    private fun fillSubject(
+        container: LinearLayout,
+        sub: Person
+    ) {
+        container.addView(TextView(this).also { it.text = "Subject:" })
+        container.addView(TextView(this).also { it.text = "  Name: ${sub.n}" })
+        container.addView(TextView(this).also { it.text = "  DoB: ${sub.dob}" })
+        sub.id?.let { idList ->
+            idList.forEach { entry ->
+                entry?.let { id ->
+                    container.addView(TextView(this).also { it.text = "  Identifier:" })
+                    container.addView(TextView(this).also { it.text = "      ${id.t} = ${id.i}" })
+                }
+            }
+        }
+    }
+
+    private fun fillRecovery(
+        container: LinearLayout,
+        rec: PastInfection
+    ) {
+        container.addView(TextView(this).also { it.text = "Recovery statement:" })
+        container.addView(TextView(this).also { it.text = "  Disease: ${rec.dis}" })
+        container.addView(TextView(this).also { it.text = "  Date: ${rec.dat}" })
+        container.addView(TextView(this).also { it.text = "  Country: ${rec.cou}" })
+    }
+
+    private fun fillTest(
+        container: LinearLayout,
+        tst: Test
+    ) {
+        container.addView(TextView(this).also { it.text = "Test:" })
+        container.addView(TextView(this).also { it.text = "  Disease: ${tst.dis}" })
+        container.addView(TextView(this).also { it.text = "  Type: ${tst.typ}" })
+        container.addView(TextView(this).also { it.text = "  Name: ${tst.tna}" })
+        container.addView(TextView(this).also { it.text = "  Manufacturer: ${tst.tma}" })
+        container.addView(TextView(this).also { it.text = "  Sample origin: ${tst.ori}" })
+        container.addView(TextView(this).also { it.text = "  Date: ${tst.dat}" })
+        container.addView(TextView(this).also { it.text = "  Result: ${tst.res}" })
+        container.addView(TextView(this).also { it.text = "  Facility: ${tst.fac}" })
+        container.addView(TextView(this).also { it.text = "  Country: ${tst.cou}" })
+    }
+
+    private fun fillVac(
+        container: LinearLayout,
+        vac: Vaccination
+    ) {
+        container.addView(TextView(this).also { it.text = "Vaccination:" })
+        container.addView(TextView(this).also { it.text = "  Disease: ${vac.dis}" })
+        container.addView(TextView(this).also { it.text = "  Vaccine: ${vac.des}" })
+        container.addView(TextView(this).also { it.text = "  Product: ${vac.nam}" })
+        container.addView(TextView(this).also { it.text = "  Authorisation Holder: ${vac.aut}" })
+        container.addView(TextView(this).also { it.text = "  Dose sequence: ${vac.seq}" })
+        container.addView(TextView(this).also { it.text = "  Total number of doses: ${vac.tot}" })
+        container.addView(TextView(this).also { it.text = "  Batch: ${vac.lot}" })
+        container.addView(TextView(this).also { it.text = "  Date: ${vac.dat}" })
+        container.addView(TextView(this).also { it.text = "  Administering centre: ${vac.adm}" })
+        container.addView(TextView(this).also { it.text = "  Country: ${vac.cou}" })
+    }
+
+    private fun getChain() = CborProcessingChain(
         CborService(VerificationCryptoService("https://dev.a-sit.at/certservice/cert")),
         ValSuiteService(),
         CompressorService(),
