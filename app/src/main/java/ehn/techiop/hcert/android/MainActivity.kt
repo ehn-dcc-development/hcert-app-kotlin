5cd462a33aeb2b014f7154d489cb1da8bb077f60
Christian Kollmann <christian.kollmann@a-sit.at>
2021-05-28 14:12:19 +0200

    Remove unused code and resources

--- app/src/main/java/ehn/techiop/hcert/android/MainActivity.kt
+++ app/src/main/java/ehn/techiop/hcert/android/MainActivity.kt
@@ -3,8 +3,6 @@ package ehn.techiop.hcert.android
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -262,19 +260,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
