ab8ab7962665136f03bec3dd585e5f89fe9f2b50
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-25 08:35:40 +0200

    Add option to pre-load settings

--- app/src/main/java/ehn/techiop/hcert/android/SettingsActivity.kt
+++ app/src/main/java/ehn/techiop/hcert/android/SettingsActivity.kt
@@ -5,7 +5,9 @@ import android.text.InputType
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -40,6 +42,70 @@ class SettingsActivity : AppCompatActivity() {
             findPreference<EditTextPreference>(KEY_TRUST_LIST_SIGNATURE)?.setOnBindEditTextListener { editText ->
                 editText.inputType = InputType.TYPE_TEXT_VARIATION_URI
             }
+            findPreference<Preference>(KEY_DEFAULTS_ASIT)?.setOnPreferenceClickListener {
+                PreferenceManager.getDefaultSharedPreferences(activity).edit()
+                    .putString(KEY_TRUST_LIST_CONTENT, "https://dgc.a-sit.at/ehn/cert/listv2")
+                    .putString(KEY_TRUST_LIST_SIGNATURE, "https://dgc.a-sit.at/ehn/cert/sigv2")
+                    .putString(
+                        KEY_TRUST_LIST_ROOT, "-----BEGIN CERTIFICATE-----\n" +
+                                "MIIBJTCBy6ADAgECAgUAwvEVkzAKBggqhkjOPQQDAjAQMQ4wDAYDVQQDDAVFQy1N\n" +
+                                "ZTAeFw0yMTA0MjMxMTI3NDhaFw0yMTA1MjMxMTI3NDhaMBAxDjAMBgNVBAMMBUVD\n" +
+                                "LU1lMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE/OV5UfYrtE140ztF9jOgnux1\n" +
+                                "oyNO8Bss4377E/kDhp9EzFZdsgaztfT+wvA29b7rSb2EsHJrr8aQdn3/1ynte6MS\n" +
+                                "MBAwDgYDVR0PAQH/BAQDAgWgMAoGCCqGSM49BAMCA0kAMEYCIQC51XwstjIBH10S\n" +
+                                "N701EnxWGK3gIgPaUgBN+ljZAs76zQIhAODq4TJ2qAPpFc1FIUOvvlycGJ6QVxNX\n" +
+                                "EkhRcgdlVfUb\n" +
+                                "-----END CERTIFICATE-----"
+                    )
+                    .apply()
+                onCreatePreferences(savedInstanceState, rootKey)
+                true
+            }
+            findPreference<Preference>(KEY_DEFAULTS_QS)?.setOnPreferenceClickListener {
+                PreferenceManager.getDefaultSharedPreferences(activity).edit()
+                    .putString(KEY_TRUST_LIST_CONTENT, "https://dgc-trusttest.qr.gv.at/trustlist")
+                    .putString(KEY_TRUST_LIST_SIGNATURE, "https://dgc-trusttest.qr.gv.at/trustlistsig")
+                    .putString(
+                        KEY_TRUST_LIST_ROOT, "-----BEGIN CERTIFICATE-----\n" +
+                                "MIIB6zCCAZGgAwIBAgIKAXmEuohlRbR2qzAKBggqhkjOPQQDAjBQMQswCQYDVQQG\n" +
+                                "EwJBVDEPMA0GA1UECgwGQk1TR1BLMQowCAYDVQQLDAFRMQwwCgYDVQQFEwMwMDEx\n" +
+                                "FjAUBgNVBAMMDUFUIERHQyBDU0NBIDEwHhcNMjEwNTE5MTMwNDQ3WhcNMjIwNjE5\n" +
+                                "MTMwNDQ3WjBRMQswCQYDVQQGEwJBVDEPMA0GA1UECgwGQk1TR1BLMQowCAYDVQQL\n" +
+                                "DAFRMQ8wDQYDVQQFEwYwMDEwMDExFDASBgNVBAMMC0FUIERHQyBUTCAxMFkwEwYH\n" +
+                                "KoZIzj0CAQYIKoZIzj0DAQcDQgAE29KpT1eIKsy5Jx3J0xpPLW+fEBF7ma9943/j\n" +
+                                "4Z+o1TytLVok9cWjsdasWCS/zcRyAh7HBL+oyMWdFBOWENCQ76NSMFAwDgYDVR0P\n" +
+                                "AQH/BAQDAgeAMB0GA1UdDgQWBBQYmsL5sXTdMCyW4UtP5BMxq+UAVzAfBgNVHSME\n" +
+                                "GDAWgBR2sKi2xkUpGC1Cr5ehwL0hniIsJzAKBggqhkjOPQQDAgNIADBFAiBse17k\n" +
+                                "F5F43q9mRGettRDLprASrxsDO9XxUUp3ObjcWQIhALfUWnserGEPiD7Pa25tg9lj\n" +
+                                "wkrqDrMdZHZ39qb+Jf/E\n" +
+                                "-----END CERTIFICATE-----"
+                    )
+                    .apply()
+                onCreatePreferences(savedInstanceState, rootKey)
+                true
+            }
+            findPreference<Preference>(KEY_DEFAULTS_PROD)?.setOnPreferenceClickListener {
+                PreferenceManager.getDefaultSharedPreferences(activity).edit()
+                    .putString(KEY_TRUST_LIST_CONTENT, "https://dgc-trust.qr.gv.at/trustlist")
+                    .putString(KEY_TRUST_LIST_SIGNATURE, "https://dgc-trust.qr.gv.at/trustlistsig")
+                    .putString(
+                        KEY_TRUST_LIST_ROOT, "-----BEGIN CERTIFICATE-----\n" +
+                                "MIIB1DCCAXmgAwIBAgIKAXnM+Z3eG2QgVzAKBggqhkjOPQQDAjBEMQswCQYDVQQG\n" +
+                                "EwJBVDEPMA0GA1UECgwGQk1TR1BLMQwwCgYDVQQFEwMwMDExFjAUBgNVBAMMDUFU\n" +
+                                "IERHQyBDU0NBIDEwHhcNMjEwNjAyMTM0NjIxWhcNMjIwNzAyMTM0NjIxWjBFMQsw\n" +
+                                "CQYDVQQGEwJBVDEPMA0GA1UECgwGQk1TR1BLMQ8wDQYDVQQFEwYwMDEwMDExFDAS\n" +
+                                "BgNVBAMMC0FUIERHQyBUTCAxMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEl2tm\n" +
+                                "d16CBHXwcBN0r1Uy+CmNW/b2V0BNP85y5N3JZeo/8l9ey/jIe5mol9fFcGTk9bCk\n" +
+                                "8zphVo0SreHa5aWrQKNSMFAwDgYDVR0PAQH/BAQDAgeAMB0GA1UdDgQWBBRTwp6d\n" +
+                                "cDGcPUB6IwdDja/a3ncM0TAfBgNVHSMEGDAWgBQfIqwcZRYptMGYs2Nvv90Jnbt7\n" +
+                                "ezAKBggqhkjOPQQDAgNJADBGAiEAlR0x3CRuQV/zwHTd2R9WNqZMabXv5XqwHt72\n" +
+                                "qtgnjRgCIQCZHIHbCvlgg5uL8ZJQzAxLavqF2w6uUxYVrvYDj2Cqjw==\n" +
+                                "-----END CERTIFICATE-----"
+                    )
+                    .apply()
+                onCreatePreferences(savedInstanceState, rootKey)
+                true
+            }
         }
     }
 
@@ -47,6 +113,9 @@ class SettingsActivity : AppCompatActivity() {
         val KEY_TRUST_LIST_CONTENT = "trust_list_content"
         val KEY_TRUST_LIST_SIGNATURE = "trust_list_signature"
         val KEY_TRUST_LIST_ROOT = "trust_list_root"
+        val KEY_DEFAULTS_ASIT = "defaults_asit"
+        val KEY_DEFAULTS_QS = "defaults_qs"
+        val KEY_DEFAULTS_PROD = "defaults_prod"
     }
 
 }
